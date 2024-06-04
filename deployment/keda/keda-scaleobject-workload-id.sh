c87962748500458894d5dfc1c6941362d63a3f07
Russell de Pina <rdepina@microsoft.com>
2024-06-04 09:28:23 -0500

    cosmetic cleanup; change activation queue length

--- deployment/keda/keda-scaleobject-workload-id.sh
+++ deployment/keda/keda-scaleobject-workload-id.sh
@@ -3,6 +3,15 @@
 # Load environment variables
 #. ./deployment/environmentVariables.sh
 
+# echo color
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+CYAN=$(tput setaf 6)
+BLUE=$(tput setaf 4)
+YELLOW=$(tput setaf 3)
+MAGENTA=$(tput setaf 5)
+NC=$(tput sgr0)
+
 echo ${YELLOW} "$(date '+%Y-%m-%d %H:%M:%S%:z') Get AKS cluster credentials" ${NC}
 az aks get-credentials --resource-group $RESOURCE_GROUP --name $AKS_CLUSTER_NAME
 if [ $? -ne 0 ]; then
@@ -23,21 +32,19 @@ else
   fi
 fi
 
-
 # get the workload identity client id
 if [ -z "$WORKLOAD_MANAGED_IDENTITY_NAME" ]; then
-  echo "WORKLOAD_MANAGED_IDENTITY_NAME is not set. Make sure you have run deployment/environmentVariables.sh"
+  echo ${RED} "WORKLOAD_MANAGED_IDENTITY_NAME is not set. Make sure you have run deployment/environmentVariables.sh" ${NC}
   exit 1
 else
-  if ! az identity show -g $AZURE_RESOURCE_GROUP -n $WORKLOAD_MANAGED_IDENTITY_NAME --query clientId -o tsv >/dev/null; then
-    echo "Workload Identity $WORKLOAD_MANAGED_IDENTITY_NAME does not exist in resource group $AZURE_RESOURCE_GROUP. Did you run deployment/all-azure/deploy.sh?"
+  if ! az identity show -g $RESOURCE_GROUP -n $WORKLOAD_MANAGED_IDENTITY_NAME --query clientId -o tsv >/dev/null; then
+    echo ${RED} "Workload Identity $WORKLOAD_MANAGED_IDENTITY_NAME does not exist in resource group $AZURE_RESOURCE_GROUP. Did you run the deployment script?" ${NC}
     exit 1
   else
     echo "Getting workload identity client id for $WORKLOAD_MANAGED_IDENTITY_NAME"
-    workloadManagedIdentityClientId=$(az identity show -g $AZURE_RESOURCE_GROUP -n $WORKLOAD_MANAGED_IDENTITY_NAME --query clientId -o tsv)
+    workloadManagedIdentityClientId=$(az identity show -g $RESOURCE_GROUP -n $WORKLOAD_MANAGED_IDENTITY_NAME --query clientId -o tsv)
   fi
 fi
-
 
 echo ${YELLOW} "$(date '+%Y-%m-%d %H:%M:%S%:z') Deploying KEDA scale object using workload ID for auth" ${NC}
 #cat >>./deployment/keda/keda-python-app.yaml <<EOF
@@ -50,7 +57,7 @@ metadata:
 spec:
   scaleTargetRef:
     name: ${AQS_TARGET_DEPLOYMENT}     #K8s deployement to target
-  minReplicaCount: 1  # We don't want pods if the queue is empty nginx-deployment
+  minReplicaCount: 0  # We don't want pods if the queue is empty nginx-deployment
   maxReplicaCount: 15 # We don't want to have more than 15 replicas
   pollingInterval: 30 # How frequently we should go for metrics (in seconds)
   cooldownPeriod:  10 # How many seconds should we wait for downscale  
@@ -62,7 +69,7 @@ spec:
       queueName: ${AZURE_QUEUE_NAME}
       accountName: ${AZURE_STORAGE_ACCOUNT_NAME}
       queueLength: '5'
-      activationQueueLength: '50' # threshold for when the scaler is active
+      activationQueueLength: '20' # threshold for when the scaler is active
       cloud: AzurePublicCloud
 ---
 apiVersion: keda.sh/v1alpha1
