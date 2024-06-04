5b44c9d81e7dc326ce9a1a11f52f7b13ebfe387b
Russell de Pina <rdepina@microsoft.com>
2024-06-04 09:40:46 -0500

    cleanup

--- deployment/environmentVariables.sh
+++ deployment/environmentVariables.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo "Setting environment variables"
 
-
 #K8s Variables
 export CLUSTER_NAME="aks-cluster"
 export K8sversion="1.28"
@@ -17,10 +16,7 @@ export AQS_TARGET_NAMESPACE="aqs-demo"
 
 #****************** ADD VALUES FOR THE FOLLOWING ENVIROMENT VARIABLES ***********************
 export AZURE_STORAGE_ACCOUNT_NAME="<storage account name>"
-# export AZURE_STORAGE_ACCOUNT_KEY="<storage account key>"
-# export AZURE_STORAGE_CONNECTION_STRING="<storage account connection string>"
 export AZURE_QUEUE_NAME="aws2azmsgs"
-# export AZURE_COSMOSDB_CONNECTION_STRING="<cosmosdb connection string>"
 export AZURE_COSMOSDB_ACCOUNT_NAME="<cosmosdb account name>"
 export AZURE_COSMOSDB_TABLE="aws2azpmts"
 #********************************************************************************************
@@ -30,7 +26,6 @@ export TAGS="owner=aksdemo"
 export STORAGE_ACCOUNT_SKU="Standard_LRS"
 export ACR_SKU="Basic"
 export AKS_NODE_COUNT=3
-
 
 # echo color
 RED=$(tput setaf 1)
